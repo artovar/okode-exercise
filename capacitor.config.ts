d4f725f909881fd7c1e86241f11a0691b5181562
Alejandro Arenas <aarenast15@gmail.com>
2022-05-11 13:55:29 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'okode-exercise',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
